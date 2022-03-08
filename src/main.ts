31f07d820cb44c7bfbc68208d87252adc0b6a14b
Esteban Canteros <estebanacanteros@gmail.com>
2022-03-08 13:29:48 -0300

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger()
+  const port = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule);
   
+  //TODO: configure origins
+  app.enableCors()
+
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(process.env.PORT);
+  await app.listen(port);
+
+  logger.log(`App running on port ${port}`)
 }
 bootstrap();
