7ac591f1683b8845951c8e14a8f099654e1d3c5c
Esteban Canteros <estebanacanteros@gmail.com>
2022-03-22 19:50:30 -0300

    renew token endpoint added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Param, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { LoginDTO } from './dto/loginDTO.dto'
 import { SignupDTO } from './dto/signupDTO'
@@ -9,12 +9,16 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/login')
-  login(@Body() loginDTO: LoginDTO): Promise<{token: string, user: User}> {
+  login(@Body() loginDTO: LoginDTO): Promise<{ token: string; user: User }> {
     return this.authService.login(loginDTO)
   }
 
   @Post('/signup')
   signup(@Body() signupDTO: SignupDTO): Promise<User> {
     return this.authService.signup(signupDTO)
+  }
+  @Post('/renew-token')
+  renewToken(@Body('token') token: string): Promise<{ user: User; token: string }> {
+    return this.authService.renewToken(token)
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm'
 import { LoginDTO } from './dto/loginDTO.dto'
@@ -23,7 +23,7 @@ export class AuthService {
     if(user && ( await bcrypt.compare(password, user.password) ) ){
       const payload: JwtPayload = { username }
       
-      const token = await this.jwtService.sign(payload)
+      const token = this.jwtService.sign(payload)
       delete user.password
       return {token, user};
     } else {
@@ -33,5 +33,17 @@ export class AuthService {
   
   async signup(signupDTO: SignupDTO): Promise<User> {    
     return this.userRepository.createUser(signupDTO)
+  }
+
+  async renewToken(token: string): Promise<{ user: User, token: string }>{
+    try {
+      const payload: JwtPayload = await this.jwtService.verify(token)
+      const user = await this.userRepository.getUserByUsername(payload.username)
+      delete user.password
+      const newToken = this.jwtService.sign({username: payload.username})
+      return { user, token: newToken }
+    } catch (error) {
+      throw new BadRequestException('Invalid token')
+    }
   }
 }
