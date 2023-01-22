b9cdd7cc77c8c5e44db1a9115d71668cdaab2620
Esteban Canteros <estebanacanteros@gmail.com>
2023-01-22 18:37:54 -0300

    enable transformation on validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const logger = new Logger()
   const port = process.env.PORT || 4000
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
   app.setGlobalPrefix('api')
-  
+
   //TODO: configure origins
   app.enableCors()
 
-  app.useGlobalPipes(new ValidationPipe())
-  await app.listen(port);
+  app.useGlobalPipes(
+    new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true } })
+  )
+  await app.listen(port)
 
   logger.log(`App running on port ${port}`)
 }
-bootstrap();
+bootstrap()
